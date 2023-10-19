efb45ca9b8db8d7e6df1e72980bb8d550e3d5a03
Jay Motamarry <79808791+jmotamarry@users.noreply.github.com>
2023-10-19 15:58:19 -0700

    working cnn with array output of discrete numbers

--- ml.py
+++ ml.py
@@ -1,6 +1,5 @@
 import numpy as np  # linear algebra
 import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
-import tf as tf
 
 from keras.models import Sequential
 from keras.layers import Dense, Conv2D, Flatten
@@ -14,12 +13,92 @@ from sklearn.metrics import mean_absolute_error
 
 import joblib
 
+
+def get_high_index(values):
+
+    values_nums = []
+
+    for j in range(len(values)):
+
+        max_value = values[j][0]
+        max_index = 0
+
+        for i in range(1, len(values[j])):
+            if values[j][i] > max_value:
+                max_value = values[j][i]
+                max_index = i
+
+        values_nums.append(max_index)
+
+    return values_nums
+
+
+def cnn(train_X, val_X, train_y, val_y):
+
+    train_X_array = np.array(train_X)
+    val_X_array = np.array(val_X)
+
+    train_y_array = np.array(train_y)
+    val_y_array = np.array(val_y)
+
+    train_X_array = train_X_array.reshape(59999, 28, 28, 1)
+    val_X_array = val_X_array.reshape(9999, 28, 28, 1)
+
+    train_y_array = to_categorical(train_y_array)
+    val_y_array = to_categorical(val_y_array)
+
+    choice = int(input("1 to train, 2 to load: "))
+    file = "data/" + input("Filename to load or dump: ") + '.keras'
+
+    if choice == 1:
+
+        # create model
+        model = Sequential()
+
+        # add model layers
+        model.add(Conv2D(64, kernel_size=3, activation='relu', input_shape=(28, 28, 1)))
+        model.add(Conv2D(32, kernel_size=3, activation='relu'))
+        model.add(Flatten())
+        model.add(Dense(10, activation='softmax'))
+
+        # compile model using accuracy to measure model performance
+        model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+        # model.summary()
+        # model.get_weights()
+
+        model.fit(train_X_array, train_y_array, validation_data=(val_X_array, val_y_array), epochs=3, verbose=1)
+
+        open(file, 'w').close()
+        model.save(file)
+
+        cnn_predictions = model.predict(val_X_array)
+        cnn_mae = mean_absolute_error(cnn_predictions, val_y_array)
+
+        print("Validation MAE for CNN Model: {:,.5f}".format(cnn_mae))
+
+    elif choice == 2:
+        index = 30
+
+        ld_model = load_model(file)
+
+        four_d = val_X_array[index:index + 2][:][:]
+
+        # print(four_d.shape)
+        # three_d = val_X_array[index][:][:].reshape(28, 28, 1)
+        # plt.imshow(three_d)
+        # plt.show()
+        # print(ld_model.predict(four_d))
+
+        print(get_high_index(ld_model.predict(four_d)))
+
+
 def regression_tree(train_X, val_X, train_y, val_y):
 
     rf_model = RandomForestRegressor(n_jobs=-1, n_estimators=60, random_state=1, verbose=2)
 
     choice = int(input("1 to train, 2 to load: "))
-    file = 'data/' + input("Filename to load or dump: ")
+    file = 'data/' + input("Filename to load or dump: ") + ".txt"
 
     if choice == 1:
         rf_model.fit(train_X, train_y)
@@ -52,60 +131,12 @@ train.rename(columns={train_cols[i]: str(i) for i in range(len(train_cols))}, in
 val_cols = list(val.columns)
 val.rename(columns={val_cols[i]: str(i) for i in range(len(val_cols))}, inplace=True)
 
-train_X = train[train.columns[1:len(train.columns)]] / 255.0
-val_X = val[val.columns[1:len(val.columns)]] / 255.0
+t_X = train[train.columns[1:len(train.columns)]] / 255.0
+v_X = val[val.columns[1:len(val.columns)]] / 255.0
 
-# print(train_X.loc[59999, '1'])
-# print(val_X.loc[:, '1'])
+t_y = train['0']
+v_y = val['0']
 
-train_y = train['0']
-val_y = val['0']
+# regression_tree(t_X, v_X, t_y, v_y)
 
-train_X_array = np.array(train_X)
-val_X_array = np.array(val_X)
-
-train_y_array = np.array(train_y)
-val_y_array = np.array(val_y)
-
-train_X_array = train_X_array.reshape(59999, 28, 28, 1)
-val_X_array = val_X_array.reshape(9999, 28, 28, 1)
-
-train_y_array = to_categorical(train_y_array)
-val_y_array = to_categorical(val_y_array)
-
-# plt.imshow(train_X_array[59998])
-# plt.show()
-
-# create model
-model = Sequential()
-
-# add model layers
-model.add(Conv2D(64, kernel_size=3, activation='relu', input_shape=(28, 28, 1)))
-model.add(Conv2D(32, kernel_size=3, activation='relu'))
-model.add(Flatten())
-model.add(Dense(10, activation='softmax'))
-
-# compile model using accuracy to measure model performance
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-
-model.summary()
-model.get_weights()
-
-choice = int(input("1 to train, 2 to load: "))
-file = "data/" + input("Filename to load or dump: ") + '.keras'
-
-if choice == 1:
-    model.fit(train_X_array, train_y_array, validation_data=(val_X_array, val_y_array), epochs=3, verbose=1)
-    open(file, 'w').close()
-    model.save(file)
-    cnn_predictions = model.predict(val_X_array)
-    print(model.predict(val_X_array[:3]))
-
-elif choice == 2:
-    ld_model = load_model(file)
-    cnn_predictions = ld_model.predict(val_X_array)
-    print(ld_model.predict(val_X_array[:3]))
-
-cnn_mae = mean_absolute_error(cnn_predictions, val_y_array)
-
-print("Validation MAE for CNN Model: {:,.5f}".format(cnn_mae))
+cnn(t_X, v_X, t_y, v_y)
