82073685dcb70a8c9df28a6cd42668219d1f9e70
Jay Motamarry <79808791+jmotamarry@users.noreply.github.com>
2023-10-19 02:00:34 -0700

    make cnn model

--- ml.py
+++ ml.py
@@ -1,8 +1,12 @@
 import numpy as np  # linear algebra
 import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
-import tensorflow as tf
+import tf as tf
 
-from tensorflow.keras import datasets, layers, models
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, Flatten
+from keras.utils import to_categorical
+from tensorflow.python.estimator import keras
+
 import matplotlib.pyplot as plt
 
 from sklearn.ensemble import RandomForestRegressor
@@ -43,23 +47,61 @@ train = pd.read_csv('C:/mnist_train.csv')
 val = pd.read_csv('C:/mnist_test.csv')
 
 train_cols = list(train.columns)
-train.rename(columns={train_cols[i]:str(i) for i in range(len(train_cols))}, inplace=True)
+train.rename(columns={train_cols[i]: str(i) for i in range(len(train_cols))}, inplace=True)
 
 val_cols = list(val.columns)
-val.rename(columns={val_cols[i]:str(i) for i in range(len(val_cols))}, inplace=True)
+val.rename(columns={val_cols[i]: str(i) for i in range(len(val_cols))}, inplace=True)
 
 train_X = train[train.columns[1:len(train.columns)]] / 255.0
 val_X = val[val.columns[1:len(val.columns)]] / 255.0
 
+# print(train_X.loc[59999, '1'])
+# print(val_X.loc[:, '1'])
+
 train_y = train['0']
 val_y = val['0']
 
-# regression_tree(train_X, val_X, train_y, val_y)
-
 train_X_array = np.array(train_X)
+val_X_array = np.array(val_X)
 
-for i in range(784):
-    print(f"{train_X_array[49035][i]:.0f}", end='')
-    if (i + 1) % 28 == 0 and i != 0:
-        print('')
+train_y_array = np.array(train_y)
+val_y_array = np.array(val_y)
 
+train_X_array = train_X_array.reshape(59999, 28, 28, 1)
+val_X_array = val_X_array.reshape(9999, 28, 28, 1)
+
+train_y_array = to_categorical(train_y_array)
+val_y_array = to_categorical(val_y_array)
+
+# plt.imshow(train_X_array[59998])
+# plt.show()
+
+# create model
+model = Sequential()
+
+# add model layers
+model.add(Conv2D(64, kernel_size=3, activation='relu', input_shape=(28, 28, 1)))
+model.add(Conv2D(32, kernel_size=3, activation='relu'))
+model.add(Flatten())
+model.add(Dense(10, activation='softmax'))
+
+# compile model using accuracy to measure model performance
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+choice = int(input("1 to train, 2 to load: "))
+file = 'data/' + input("Filename to load or dump: ") + '.keras'
+
+if choice == 1:
+    model.fit(train_X_array, train_y_array, validation_data=(val_X_array, val_y_array), epochs=3, verbose=1)
+    model.save(file)
+    cnn_predictions = model.predict(val_X_array)
+
+elif choice == 2:
+    ld_model = tf.keras.models.load_model(file)
+    cnn_predictions = ld_model.predict(val_X_array)
+
+cnn_mae = mean_absolute_error(cnn_predictions, val_y_array)
+
+print("Validation MAE for CNN Model: {:,.5f}".format(cnn_mae))
+
+print(model.predict(val_X_array[:3]))
